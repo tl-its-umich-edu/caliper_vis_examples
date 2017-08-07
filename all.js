d6a77d60c1397b2cbf0288cc3966ec2eb8fc907b
Silverio <gsilver@umich.edu>
2017-08-07 10:16:01 -0400

    multiple bar chart, x: courses, y:event counts

--- all.js
+++ all.js
@@ -1,30 +1,97 @@
 $.getJSON('all_anon_compacted.json', {})
   .done(function(data) {
 
-
-    historicalBarChart = [{
-      values: []
-    }];
-
-
-    raw = data;
-
-    
-    var actions = _.pluck(raw, 'action');
-    var actions_uniq = _.uniq(_.pluck(raw, 'action'));
-    var result = _.map(actions_uniq, function(action) {
-      var length = _.reject(actions, function(el) {
-        return (el.indexOf(action) < 0);
-      }).length;
-      return {
-        id: action,
-        count: length
-      };
+    $('.btn').on('click', function(e){
+      $(".btn-group button").each(function() {
+        $(this).attr('disabled',false).attr('class','btn-default btn');
+      });
+      $(this).attr('disabled',true).attr('class','btn-primary btn');
     });
 
-    historicalBarChart[0].values = result;
-    update();
-    function update() {
+
+    $('#actions').on('click', function(e) {
+      bar_chart = [{
+        values: []
+      }];
+
+
+      var actions = _.pluck(data, 'action');
+      var actions_uniq = _.uniq(_.pluck(data, 'action'));
+
+      var result = _.map(actions_uniq, function(action) {
+        var length = _.reject(actions, function(el) {
+          return (el.indexOf(action) < 0);
+        }).length;
+        return {
+          id: action,
+          count: length
+        };
+      });
+
+      bar_chart[0].values = result;
+      updateBarChart();
+    });
+
+    $('#assigItems').on('click', function(e) {
+      d3.selectAll("svg > *").remove();
+      var grouped_data = [];
+      var actions_uniq = _.uniq(_.pluck(data, 'action'));
+
+      _.each(actions_uniq, function(action,i){
+        grouped_data.push({
+          key:action,
+          values:[]
+        });
+      });
+
+      _.each(data, function(item,i) {
+        var theKey = _.findWhere(grouped_data, {key: item.action});
+        var theItem = _.findWhere(theKey.values, {x: item.class});
+
+        if(theItem) {
+          theItem.y = theItem.y + 1;
+        }
+        else {
+          theKey.values.push({
+            x:item.class,
+            y:0,
+            series:item.action
+          });
+        }
+      });
+
+      d3.selectAll("svg > *").remove();
+      nv.addGraph(function() {
+          var chart = nv.models.multiBarChart()
+            //.transitionDuration(350)
+            .reduceXTicks(true)   //If 'false', every single x-axis tick label will be rendered.
+            .rotateLabels(0)      //Angle to rotate x-axis labels.
+            .showControls(false)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
+            .groupSpacing(0.1)    //Distance between each group of bars.
+          ;
+          chart.yAxis
+              .tickFormat(d3.format(',.1f'));
+
+          d3.select('#chart svg')
+              .datum(grouped_data)
+              .call(chart);
+
+          nv.utils.windowResize(chart.update);
+
+          return chart;
+      });
+
+
+    });
+
+
+
+    $('#actions').trigger("click");
+
+    // different visualizations
+
+    function updateBarChart() {
+      d3.selectAll("svg > *").remove();
       nv.addGraph(function() {
         var chart = nv.models.discreteBarChart()
           .x(function(d) {
@@ -35,7 +102,8 @@ $.getJSON('all_anon_compacted.json', {})
           })
           .margin({
             "top": 10,
-            "bottom": 200
+            "bottom": 150,
+            "left":100
           })
           .showValues(false)
           .duration(250);
@@ -46,7 +114,7 @@ $.getJSON('all_anon_compacted.json', {})
 
 
         d3.select('#chart svg')
-          .datum(historicalBarChart)
+          .datum(bar_chart)
           .call(chart);
 
         nv.utils.windowResize(chart.update);
