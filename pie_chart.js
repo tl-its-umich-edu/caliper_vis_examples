7bce988e6880360bdb7f0cbe200646ec05de75d1
Gonzalo Silverio <gonzalo.silverio@gmail.com>
2017-08-21 16:53:58 -0400

    add a filter to pie chart

--- pie_chart.js
+++ pie_chart.js
@@ -19,30 +19,48 @@ $.getJSON('raw.json', {})
         });
       }
     });
-
+    problem_sets_core = _.sortBy(problem_sets, 'label');
     problem_sets = _.sortBy(problem_sets, 'label');
 
-    var chart;
-    //Donut chart example
-    nv.addGraph(function() {
-      chart = nv.models.pieChart()
-          .x(function(d) { return d.label })
-          .y(function(d) { return d.value })
-          .showLabels(true)
-          .labelThreshold(0.05)
-          .labelType("percent")
-          .donut(true)
-          .donutRatio(0.35);
 
-        d3.select("#chart svg")
-            .datum(problem_sets)
-            .transition().duration(350)
-            .call(chart);
+    var updatePie = function(){
+      var chart;
+      //Donut chart example
+      nv.addGraph(function() {
+        chart = nv.models.pieChart()
+            .x(function(d) { return d.label })
+            .y(function(d) { return d.value })
+            .showLabels(true)
+            .labelThreshold(0.05)
+            .labelType("percent")
+            .donut(true)
+            .donutRatio(0.35);
+
+          d3.select("#chart svg")
+              .datum(problem_sets)
+              .transition().duration(350)
+              .call(chart);
 
 
-        nv.utils.windowResize(chart.update);
-        return chart;
-    });
+          nv.utils.windowResize(chart.update);
+          return chart;
+      });
+    };
+
+
+        updatePie();
+
+        $('input[type=radio]').change(function() {
+          if(this.value ==='tenmost'){
+            problem_sets = _.first(_.sortBy(problem_sets, 'value').reverse(), 10);
+          } else if (this.value ==='tenleast'){
+            problem_sets = _.first(_.sortBy(problem_sets, 'value'), 10);
+          } else {
+            problem_sets = problem_sets_core;
+          }
+          updatePie();
+        });
+
 
 
 
